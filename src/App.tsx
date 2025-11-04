fa09c40c5676d680dc57a5d133a4477d4ca87995
Dimas Darma <dimasdarma35@gmail.com>
2025-11-04 09:40:57 +0700

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Products from './components/Products';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Header />
+      <Hero />
+      <About />
+      <Products />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
